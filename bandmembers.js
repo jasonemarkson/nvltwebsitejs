fd0d0d9547e25fcca45cfc3e4dd7e3534fc97efe
jasonemarkson <jasonmarkson76@gmail.com>
2022-04-10 17:49:49 -0400

    minor updates

--- bandmembers.js
+++ bandmembers.js
@@ -3,5 +3,5 @@ const bandMembers = [
     { name: "Anthony Vecere", instrument: "Guitar", image: "./AV.JPEG" }, 
     { name: "Tony McHale", instrument: "Guitar/Bass", image: "./AM.JPEG" }, 
     { name: "Jason Markson", instrument: "Guitar/Bass", image: "./JM.JPEG" }, 
-    { name: "Tyler Katz", instrument: "Drums/Percussion", image: "./TK.JPEG" }
+    { name: "Tyler Katz", instrument: "Drums & Percussion", image: "./TK.JPEG" }
 ]

e08ffcb71ecac8bd7a4111f1b318082f45d3ce77
jasonemarkson <jasonmarkson76@gmail.com>
2022-04-10 17:30:19 -0400

    edited the pics for some minor touch ups

--- bandmembers.js
+++ bandmembers.js
@@ -1,7 +1,7 @@
 const bandMembers = [
     { name: "Mary Jardine", instrument: "Vocals", image: "./MJ.JPEG" }, 
-    { name: "Anthony Vecere", instrument: "Guitar", image: "./AV.JPG" }, 
-    { name: "Tony McHale", instrument: "Guitar/Bass", image: "./AM.JPG" }, 
-    { name: "Jason Markson", instrument: "Guitar/Bass", image: "./JM.JPG" }, 
-    { name: "Tyler Katz", instrument: "Drums/Percussion", image: "./TK.JPG" }
+    { name: "Anthony Vecere", instrument: "Guitar", image: "./AV.JPEG" }, 
+    { name: "Tony McHale", instrument: "Guitar/Bass", image: "./AM.JPEG" }, 
+    { name: "Jason Markson", instrument: "Guitar/Bass", image: "./JM.JPEG" }, 
+    { name: "Tyler Katz", instrument: "Drums/Percussion", image: "./TK.JPEG" }
 ]

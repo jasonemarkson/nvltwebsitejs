2924c8b661379f250278584c9ffcf64a8c9543e5
jasonemarkson <jasonmarkson76@gmail.com>
2022-04-11 20:38:46 -0400

    edited the press release and interview section to add in the delancey performance. it is now a media section too. was able to add in some nice css for when you look at each band member, it zooms in to their picture. also when you mouseover the video, the border changes colors

---
+++ notes.md
@@ -0,0 +1,5 @@
+Things to add
+
+1) Our links to instagram, youtube, facebook, emailing us at gmail
+2) Upcoming Shows
+3) Some pictures of us performing
--- src/index.js
+++ src/index.js
@@ -3,25 +3,6 @@ const addMemberForm = document.getElementById("add-member-form")
 const bandMembersDiv = document.getElementById("band-members-div")
 const memberCardClass = document.getElementsByClassName("member-card")
 
-
-// addMemberForm.addEventListener("submit", handleSubmit)
-
-// practice creating a form and adding lines
-
-// function handleSubmit(event) {
-//     event.preventDefault()
-//     const e = event.target
-//     const name = e.name.value
-//     const instruments = e.instruments.value
-
-//     membersContainer.innerHTML += `
-//     <li>
-//         ${name} - ${instruments}
-//     </li>
-//     `
-//     e.reset()
-// }
-
 function renderBandMembers() {
     bandMembers.map(member => {
         const memberDiv = document.createElement("div")

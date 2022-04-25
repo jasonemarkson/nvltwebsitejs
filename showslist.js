f5715b623bcb44a96027a74a905bdf2bf1716572
jasonemarkson <jasonmarkson76@gmail.com>
2022-04-25 13:34:33 -0400

    updates

---
+++ showslist.js
@@ -0,0 +1,14 @@
+const showsList = [{
+    venue: "The Bowery Electric",
+    date: "5/12/2022",
+    ticketLink: "https://dice.fm/partner/dice/event/rpqgv-nancy-rose-nvlt-and-more-to-come-12th-may-the-bowery-electric-new-york-tickets"
+}, {
+    venue: "The Kingsland",
+    date: "6/2/2022",
+    ticketLink: ""
+}, {
+    venue: "Connolly's Klub 45", 
+    date: "6/17/2022", 
+    ticketLink: "https://www.eventbrite.com/e/haley-faye-rosenthalnvltgate-46sex-4-breakfast-tickets-318455026557"
+}
+]
